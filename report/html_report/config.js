50d72369b638c51d07baf8d367d77555ff29f1d9
Olha Lutsenko <oleshna.27.l@gmail.com>
2026-02-23 19:05:37 +0100

    make build

---
+++ report/html_report/config.js
@@ -0,0 +1,31 @@
+report({
+  "testSuite": "BackstopJS",
+  "tests": [
+    {
+      "pair": {
+        "reference": "..\\bitmaps_reference\\Entire_document.png",
+        "test": "..\\bitmaps_test\\20260223-181611\\Entire_document.png",
+        "selector": "document",
+        "fileName": "Entire_document.png",
+        "label": "Entire document",
+        "requireSameDimensions": false,
+        "misMatchThreshold": 1,
+        "url": "http://localhost:3001/index.html",
+        "referenceUrl": "https://mate-academy.github.io/layout_solutions/hello-world/",
+        "expect": 0,
+        "viewportLabel": "custom-size",
+        "diff": {
+          "isSameDimensions": false,
+          "dimensionDifference": {
+            "width": 0,
+            "height": -296
+          },
+          "rawMisMatchPercentage": 0.9248554913294799,
+          "misMatchPercentage": "0.92",
+          "analysisTime": 16
+        }
+      },
+      "status": "pass"
+    }
+  ]
+});
